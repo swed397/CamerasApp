13ed48a5101df8ba01846d0095aae75bafda30ed
Rodion <swed197@mail.ru>
2023-12-27 02:43:33 +0500

    init + half of one lazy column

--- app/src/main/java/com/android/cameras/app/ui/main/MainScreenHolder.kt
+++ app/src/main/java/com/android/cameras/app/ui/main/MainScreenHolder.kt
@@ -1,6 +1,5 @@
 package com.android.cameras.app.ui.main
 
-import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
--- app/src/main/java/com/android/cameras/app/ui/theme/Theme.kt
+++ app/src/main/java/com/android/cameras/app/ui/theme/Theme.kt
@@ -2,7 +2,6 @@ package com.android.cameras.app.ui.theme
 
 import android.app.Activity
 import android.os.Build
-import android.view.WindowManager
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
