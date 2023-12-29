2063e0ac52165ffa3f7991b300e12066ebf5c036
Rodion <swed197@mail.ru>
2023-12-29 20:39:06 +0500

    added ui model mapper

--- app/src/main/java/com/android/cameras/app/ui/cameras/CamerasState.kt
+++ app/src/main/java/com/android/cameras/app/ui/cameras/CamerasState.kt
@@ -1,7 +1,5 @@
 package com.android.cameras.app.ui.cameras
 
-import com.android.cameras.app.domain.CameraModel
-
 sealed interface CamerasState {
     data object Loading : CamerasState
     data class Content(val data: Map<String, List<CameraUiModel>>) : CamerasState
